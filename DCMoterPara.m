af0ee2669e60d3d5686be850d1c84648bf06c548
studyHooligen <2373180028@qq.com>
2020-06-10 20:52:04 +0800

    DCMwithPID
    本次提交的内容为：使用PID闭环控制的直流电机系统，以及参数设置脚本

---
+++ DCMoterPara.m
@@ -0,0 +1,8 @@
+% 注：本脚本内参数全部都采用国际标准单位
+% 作者：studyHooligen
+Ra = 0.4;   % 内阻 Resistance
+La = 13.5*10^(-6);   % 电感 Inductance
+Kt = 5.77*10^(-3);   % 转矩常数 Torque constant
+Ke = 0.6*10^(-3)/(2*pi/60);   % 电动势常数 Back-EMF constant
+b = 1e-6;    % 粘滞系数 
+Jm = 5.6/1000/10000;   % 转动惯量 Rotor inertia
